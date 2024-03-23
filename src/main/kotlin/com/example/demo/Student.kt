b3fbf0cfa68c586633134fec219e4795f0b24cb4
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-23 14:10:40 +0100

    Chapter 10 "@Sequence" done

--- src/main/kotlin/com/example/demo/Student.kt
+++ src/main/kotlin/com/example/demo/Student.kt
@@ -1,11 +1,12 @@
 package com.example.demo
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity(name="Student")
 data class Student(
     @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     var id: Long,
     var firstName: String,
     var lastName: String,
