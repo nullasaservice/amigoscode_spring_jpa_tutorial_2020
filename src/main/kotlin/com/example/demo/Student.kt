1f6bd764205e4662502f889bd47713317ae6e147
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-23 14:15:54 +0100

    Chapter 11 "@Column" done

--- src/main/kotlin/com/example/demo/Student.kt
+++ src/main/kotlin/com/example/demo/Student.kt
@@ -7,9 +7,18 @@ data class Student(
     @Id
     @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @Column(name = "id", updatable = false)
     var id: Long,
+
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
     var firstName: String,
+
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
     var lastName: String,
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT", unique = true)
     var email: String,
+
+    @Column(name = "age")
     var age: Int
 )
